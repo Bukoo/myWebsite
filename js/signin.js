33a252abbb257e2225ccc2c47067ad8808e57519
Bukoo <545665926@qq.com>
2016-02-29 00:59:05 +0800

    完善登陆界面动画，添加注册界面、登陆界面的‘记住我’

--- js/signin.js
+++ js/signin.js
@@ -1,15 +1,26 @@
 function animationShow() {
 	var signin = document.getElementById('to-signin');
-	signin.onclick = curtainShow;
+	signin.onclick = signInBoxShow;
+	var signup = document.getElementById('to-signup');
+	signup.onclick = signUpBoxShow;
 }
 
-function curtainShow() {
+function signInBoxShow() {
 	var tape = document.getElementById('tape');
 	tape.parentNode.removeChild(tape);
 	var curtain = document.getElementById('curtain');
 	curtain.style.visibility = 'visible';
 	var signinBox = document.getElementById('signin-box');
-	signinBox.style.visibility = 'visible';
+	signinBox.style.display = 'block';
+	addClassName(signinBox,'fade-in');
+}
+function signUpBoxShow() {
+	var tape = document.getElementById('tape');
+	tape.parentNode.removeChild(tape);
+	var curtain = document.getElementById('curtain');
+	curtain.style.visibility = 'visible';
+	var signinBox = document.getElementById('signup-box');
+	signinBox.style.display = 'block';
 	addClassName(signinBox,'fade-in');
 }
 
