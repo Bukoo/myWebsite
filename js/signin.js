1ecfe53473f6812c753dc36fa31ca8c52c661f26
Bukoo <545665926@qq.com>
2016-03-02 16:26:40 +0800

    增加验证表单功能，添加注释

--- js/signin.js
+++ js/signin.js
@@ -1,27 +1,105 @@
 function animationShow() {
+	if (!document.getElementById) return;
+// get signIn button element
 	var signin = document.getElementById('to-signin');
 	signin.onclick = signInBoxShow;
+// get signUp button element
 	var signup = document.getElementById('to-signup');
 	signup.onclick = signUpBoxShow;
 }
 
 function signInBoxShow() {
+	if (!document.getElementById) return;
+// remove sliding tape
 	var tape = document.getElementById('tape');
 	tape.parentNode.removeChild(tape);
+// display black background
 	var curtain = document.getElementById('curtain');
 	curtain.style.visibility = 'visible';
+// display signIn box
 	var signinBox = document.getElementById('signin-box');
 	signinBox.style.display = 'block';
-	addClassName(signinBox,'fade-in');
-}
-function signUpBoxShow() {
-	var tape = document.getElementById('tape');
-	tape.parentNode.removeChild(tape);
-	var curtain = document.getElementById('curtain');
-	curtain.style.visibility = 'visible';
-	var signinBox = document.getElementById('signup-box');
-	signinBox.style.display = 'block';
+// add fade in animation for sign in box
 	addClassName(signinBox,'fade-in');
 }
 
+function signUpBoxShow() {
+	if (!document.getElementById) return;
+
+	var tape = document.getElementById('tape');
+	tape.parentNode.removeChild(tape);
+
+	var curtain = document.getElementById('curtain');
+	curtain.style.visibility = 'visible';
+
+	var signinBox = document.getElementById('signup-box');
+	signinBox.style.display = 'block';
+	
+	addClassName(signinBox,'fade-in');
+}
+
+function formatCheck(argument) {
+	if (!document.getElementById) return;
+
+	if (document.getElementById('signin-username')) {
+		userNameFormatCheck('signin-username');
+	}
+	if (document.getElementById('signup-username')) {
+		userNameFormatCheck('signup-username');
+	}
+	if (document.getElementById('signin-email')) {
+		emailFormatCheck('signin-email');
+	}
+	if (document.getElementById('signup-email')) {
+		emailFormatCheck('signup-email');
+	}
+	
+}
+
+function userNameFormatCheck(elementId) {
+	if (!document.getElementById) return;
+
+	var username, usernameTxt, patrn, usernameError;
+// get the username input box element
+	username = document.getElementById(elementId);
+// while focal point leaves username input box
+	username.onblur = function() {
+	// get username input box content
+		usernameTxt = username.value;
+	// username regular expression for only letters, numbers and underscores
+		patrn = /^\w+$/;
+	// get error message element for username input
+		usernameError = document.getElementById('error-msg-'+elementId);
+	// while not empty and not matched
+		if (usernameTxt !== '' && !usernameTxt.match(patrn)) {
+			usernameError.innerHTML = 'The Username can only contain letters, numbers and underscores!';
+	// length less than 32 characators
+		} else if(usernameTxt.length > 32) {
+			usernameError.innerHTML = 'The length of your Username should less than 32 characaters!';
+		} else {
+	// while empty or matched
+			usernameError.innerHTML = '';
+		}
+	}
+}
+
+function emailFormatCheck(elementId) {
+	if (!document.getElementById) return;
+
+	var email, emailTxt, patrn, emailError;
+	email = document.getElementById(elementId);
+	email.onblur = function() {
+		emailTxt = email.value;
+	// email regular expression
+		patrn = /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/;
+		emailError = document.getElementById('error-msg-' + elementId);
+
+		if (emailTxt !== '' && !emailTxt.match(patrn)) {
+			emailError.innerHTML = 'Sorry you don\'t input a correct email address.';
+		} else {
+			emailError.innerHTML = '';
+		}
+	}
+}
 addLoadFunction(animationShow);
+addLoadFunction(formatCheck);
